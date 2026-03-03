51503cedb71493d3740363214a2b6e81c3b6dcd7
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2026-03-03 07:05:38 +0100

    Update R2_T03_Rek_04_Komb_Ver_000.cs

--- R2_T03_Rek_04_Komb_Ver_000.cs
+++ R2_T03_Rek_04_Komb_Ver_000.cs
@@ -7,11 +7,13 @@ class R2_T03_Rek_04_Komb_Ver_000
         // Sat3();
         // Binarni_duzine_4();
 
-        int n = int.Parse(Console.ReadLine());      // N: Broj elemenata skupa
-        int k = int.Parse(Console.ReadLine());      // K: Duzina
-        // Varijacije(n, k);
-        int[] a = new int[k];
-        Varijacija_Sledeca_Izracunaj_Rekurz(a, n, k);
+        int n = int.Parse(Console.ReadLine());                  // N: Broj elemenata skupa
+        int k = int.Parse(Console.ReadLine());                  // K: Duzina
+        
+        Varijacije_Iter(n, k);                                  // Iterativno
+        
+        //int[] a = new int[k];                                 // Rekurzivno
+        //Varijacija_Sledeca_Izracunaj_Rekurz(a, n, k);         // Rekurzivno
     }
 
     static void Dvocifreni()                                    // O(N^2)
@@ -49,35 +51,36 @@ class R2_T03_Rek_04_Komb_Ver_000
                         Console.WriteLine(i + "" + j + "" + k + "" + l);
     }
 
-    static void Niz_Ispisi(int[] a)
+    static void Niz_Ispisi(int[] a)                             // IZLAZ: Ispisivanje vrednosti elemenata niza
     {
-        int n = a.Length;
+        int n = a.Length;                                       // Duzina niza
         for (int i = 0; i < n; i++) Console.Write(a[i] + " ");
         Console.WriteLine();
     }
-    static void Varijacije(int n, int k)
+    static void Varijacije_Iter(int n, int k)                   // Iterativno
     {
         int[] a = new int[k];
+        for (int i = 0; i < k; i++) a[i] = 1;                   // Ako vrednosti mogu da budu od 1 do n (a ne od 0 do n-1)
         do
             Niz_Ispisi(a);
-        while (Varijacija_Sledeca_Izracunaj(a, n));
+        while (Varijacija_Sledeca_Izracunaj(a, n));             // Dok postoji sledeca varijacija
     }
-    static bool Varijacija_Sledeca_Izracunaj(int[] a, int n)
+    static bool Varijacija_Sledeca_Izracunaj(int[] a, int n)    // Iterativno sledeca
     {
-        int k = a.Length;
-        int i = k - 1;
-        for (i = k - 1; i >= 0 && a[i] == n - 1; i--) a[i] = 0;
-        if (i < 0) return false;
-        a[i]++;
+        int k = a.Length;                                       // Duzina niza
+        int i = k - 1;                                          // Pozicija poslednjeg elementa u nizu (najmanje tezine)
+        for (i = k - 1; i >= 0 && a[i] == n; i--) a[i] = 1;     // Inicijalizacija: Prva varijacija
+        if (i < 0) return false;                                // Ako je poslednja varijacija, odnosno ne postoji sledeca varijacija
+        a[i]++;                                                 // Ako postoji sledeca varijacija
         return true;
     }
-    static void Varijacija_Sledeca_Izracunaj_Rekurz(int[] a, int n, int k)
+    static void Varijacija_Sledeca_Izracunaj_Rekurz(int[] a, int n, int k)  // Rekurzivno
     {
         if (k == 0) Niz_Ispisi(a);
         else
-            for (int nn = 0; nn < n; nn++)
+            for (int v = 1; v <= n; v++)
             {
-                a[a.Length - k] = nn;
+                a[a.Length - k] = v;
                 Varijacija_Sledeca_Izracunaj_Rekurz(a, n, k - 1); 
             }
     }
